ae47470b45d91feef165622844c9f61f2ec0a453
jaumesagues <jaumesagues@yahoo.es>
2021-06-24 19:44:54 +0200

    Tipo de material ha sido modificado para poder transformar enteros en tipo enum

--- floristeria/model/Material.java
+++ floristeria/model/Material.java
@@ -1,15 +1,27 @@
 package floristeria.model;
 
 public class Material {
-	
+
 	public enum TipoMaterial {
 
 		//aquí también podríamos utilizar valores (1 - 2) en lugar de Strings
-		MADERA ("madera"), PLASTICO("plastico");
-		
-		TipoMaterial(String tipo){
-			
+		MADERA("madera",0), PLASTICO("plastico",1);
+
+		TipoMaterial(String string, int i) {
+			// TODO Auto-generated constructor stub
 		}
+		
+		public static TipoMaterial GetTipoMaterial(int i) {
+			switch (i) {
+			case 0:
+				return TipoMaterial.MADERA;
+			case 1:
+				return TipoMaterial.PLASTICO;
+			default:
+				return null;
+			}
+		}
+		
 	}
 
 }
