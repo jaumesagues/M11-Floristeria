76893961a1ea33ce078ccb50095fc050319ef592
jaumesagues <jaumesagues@yahoo.es>
2021-06-24 19:43:06 +0200

    Interficie usando la consola está terminada. Solamente es necesario llamar al controlador

--- floristeria/view/InterficieConsola.java
+++ floristeria/view/InterficieConsola.java
@@ -2,9 +2,13 @@ package floristeria.view;
 
 import javax.swing.JOptionPane;
 
+import floristeria.model.Material;
+import floristeria.model.Material.TipoMaterial;
+
 public class InterficieConsola {
 	
 	private static Menu menu_principal;
+	private static Menu menu_materiales_decoracion;
 	
 	public InterficieConsola() {
 		CreacionMenu();
@@ -20,13 +24,13 @@ public class InterficieConsola {
 				CrearFloristeria();
 				break;
 			case 1:
-				System.out.println("Añadiendo árbol nuevo.");
+				AddArbol();
 				break;
 			case 2:
-				System.out.println("Añadiendo flor nueva.");		
+				AddFlor();		
 				break;
 			case 3:
-				System.out.println("Añadiendo decoración nueva.");
+				AddDecoracion();
 				break;
 			case 4:
 				System.out.println("Mostrar stock.");
@@ -39,12 +43,76 @@ public class InterficieConsola {
 	private void CrearFloristeria() {
 		String nombre_floristeria;
 		nombre_floristeria=pedirDato("Crear floristería", "Introducir el nombre de la floristería");
+		if (nombre_floristeria==null) return;
 		System.out.println("Floristería con nombre " + nombre_floristeria + " creada.");
 	}
 	
+	private void AddArbol() {
+		double precio, altura;
+		
+		precio=pedirDouble("Añadir árbol", "Introducir precio", -1000);
+		if (precio<0.0) return;
+		
+		altura=pedirDouble("Añadir árbol", "Introducir altura", -1000);
+		if (altura<0.0) return;
+		
+		System.out.println("Árbol con altura " + altura + " y precio " + precio +" añadido.");
+	}
+	
+	private void AddFlor() {
+		double precio;
+		String color;
+		
+		precio=pedirDouble("Añadir flor", "Introducir precio", -1000);
+		if (precio<0.0) return;
+		
+		color=pedirDato("Añadir flor", "Introducir el color");
+		if (color==null) return;
+		
+		System.out.println("Flor de color " + color + " y precio " + precio +" añadida.");
+	}
+	
+	private void AddDecoracion() {
+		double precio;
+		int decoracion_opcion;
+		TipoMaterial material;
+		
+		precio=pedirDouble("Añadir decoración", "Introducir precio", -1000);
+		if (precio<0.0) return;
+		
+		decoracion_opcion=menu_materiales_decoracion.showMenu();
+		if (decoracion_opcion==2) return;
+		material=TipoMaterial.GetTipoMaterial(decoracion_opcion);
+	
+		System.out.println("Decoración de " + material.toString() + " y precio " + precio +" añadida.");
+	}
+
 	private String pedirDato(String titol, String textSobreLaDadaAdemanar) {
 		String dada = JOptionPane.showInputDialog(null, textSobreLaDadaAdemanar, titol, JOptionPane.QUESTION_MESSAGE);
 		return dada;
+	}
+	
+	public static double pedirDouble(String titulo, String message, double nullnumber) {
+		String answer_str;
+		double answer=nullnumber;
+		boolean answer_ok=false;
+		
+		while (!answer_ok) {
+			answer_str = JOptionPane.showInputDialog(null, message, titulo, JOptionPane.QUESTION_MESSAGE);
+			if (answer_str == null) {
+				return nullnumber;
+			} else {
+				try {
+					answer = Double.parseDouble(answer_str);
+					answer_ok = true;
+				} catch (Exception e) {
+					System.out.println("El valor introduït no és correcte."
+							+ "Haurà d'introduir una altre valor o prèmer 'Cancelar'");
+				}
+			}
+		}
+		
+		return answer;
 	}
 	
 	private void CreacionMenu() {
@@ -56,5 +124,11 @@ public class InterficieConsola {
 		menu_principal.addOptionMenu("Añadir decoración");
 		menu_principal.addOptionMenu("Mostrar Stock");
 		menu_principal.addOptionMenu("Salir");
+		
+		menu_materiales_decoracion=new Menu("Escoja un material para la decoración.", 3);
+		
+		menu_materiales_decoracion.addOptionMenu("Madera");
+		menu_materiales_decoracion.addOptionMenu("Plástico");
+		menu_materiales_decoracion.addOptionMenu("Salir");
 	}
 }
