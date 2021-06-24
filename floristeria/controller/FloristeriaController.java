96f9d3584c3ca3ebfcb06ece7d5342faa3fa534a
jaumesagues <jaumesagues@yahoo.es>
2021-06-24 19:45:19 +0200

    El controlador permite crear la floristeria

--- floristeria/controller/FloristeriaController.java
+++ floristeria/controller/FloristeriaController.java
@@ -1,5 +1,22 @@
 package floristeria.controller;
 
-public class FloristeriaController {
+import floristeria.model.Floristeria;
 
+public class FloristeriaController {
+	
+	private Floristeria floristeria;
+	
+	public FloristeriaController () {
+		floristeria=null;
+	}
+	
+	public void crearFloristeria(String nombre) throws Exception {
+		if (floristeria==null) {
+			floristeria=new Floristeria(nombre);
+		} else  throw new Exception("La floristería ya ha sido creada anteriormente");
+	}
+	
+	public boolean IsFloristeriaCreada() {
+		return (floristeria!=null);
+	}
 }
